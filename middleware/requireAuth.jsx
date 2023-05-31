1c48cc93a91f4e15e344d3b7797ac21adfff1790
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-31 03:51:50 -0400

    authorizing middleware added

---
+++ middleware/requireAuth.jsx
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require('../models/userModel')
+
+const requireAuth = async (req, res, next) => {
+  //checking user auth
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Needs auth token" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    // destructuring id from payload sent from token
+    const {_id} = jwt.verify(token, process.env.SECRET);
+
+    req.user = await User.findOne({_id}).select('_id')
+    next()
+
+  } catch (error) {
+    console.log(error)
+    res.status(401).json({error: 'Req not authorized'})
+  }
+  
+};
+
+module.exports = requireAuth
--- routes/plinks.js
+++ routes/plinks.js
@@ -5,26 +5,31 @@ const {
   getAllPlinks,
   getSinglePlink,
   deletePlink,
-  updatePlink
+  updatePlink,
 } = require("../controllers/plinkControllers");
 
+const requireAuth = require('../middleware/requireAuth')
+
 const router = express.Router();
+
+//requiring auth in order to call routes
+router.use(requireAuth)
 
 //Index New Delete Update Create Edit Show
 
 //get all plinks
 router.get("/", getAllPlinks);
 
-//DELETE a plink
-router.delete("/:id", deletePlink);
+//get a single plink
+router.get("/:id", getSinglePlink);
 
 //POST a new plink
 router.post("/", createPlink);
 
+//DELETE a plink
+router.delete("/:id", deletePlink);
+
 //UPDATE a plink
 router.patch("/:id", updatePlink);
-
-//get a single plink
-router.get("/:id", getSinglePlink);
 
 module.exports = router;
