c23a7e3ad3edc08404a094acd38029deacbb9468
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-31 12:49:53 -0400

    bug

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const {signupUser, loginUser} = require('../controllers/userController')
 
 //login
-router.post('/login', loginUser)
+router.post('https://plinkify-frontend.onrender.com/login', loginUser)
 
 //signup
 
