ba88292c253ed24a338e9c0166705283a1b4c242
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-31 01:08:59 -0400

    user modelupdated/ login

--- models/userModel.js
+++ models/userModel.js
@@ -44,4 +44,25 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+//static log in method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("wrong email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("wrong password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("user", userSchema);
