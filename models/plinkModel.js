8161f3a33735d95a58eabbbe5fc3e8c3d986553a
Joshua Poveda <joshuapoveda9@gmail.com>
2023-07-21 18:35:23 -0400

    updated model.3

--- models/plinkModel.js
+++ models/plinkModel.js
@@ -16,6 +16,7 @@ const plinkSchema = new Schema(
     url8: { type: String, required: false },
     url9: { type: String, required: false },
     url10: { type: String, required: false },
+    public: {type: Boolean, default: false, required:false},
     user_id: { type: String, required: true },
   },
   { timestamps: true }
